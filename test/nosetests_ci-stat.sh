75111ab9fdc189abe52b85523e061cdff2551f71
maxwu <maxwunj@gmail.com>
2017-02-12 14:14:12 +1300

    Update circle YAML

--- test/nosetests_ci-stat.sh
+++ test/nosetests_ci-stat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nosetests --logging-level=DEBUG --debug=me.maxwu -s -v --with-xunit --xunit-file ci-stat_nose_xunit.xml --with-coverage --cover-package=me.maxwu --cover-html ./test
+nosetests --logging-level=INFO --debug=me.maxwu -s -v --with-xunit --xunit-file ci-stat_nose_xunit.xml --with-coverage --cover-package=me.maxwu --cover-html ./test
