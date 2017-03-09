7112a6e1633bc26f0112a9692fa42cc8be8bc6e8
maxwu <maxwunj@gmail.com>
2017-03-10 01:00:42 +1300

    fix the JOB_NAME missing with travis and circle

--- test/nosetests_ci-stat.sh
+++ test/nosetests_ci-stat.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 ########################
 # Step 1: Setup virtualenv
+# This step is only for Jenkins. Travis and CircleCI will ignore this step.
 ########################
 
-# Path to virtualenv cmd installed by pip
-# /usr/local/bin/virtualenv
-PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
-if [ ! -d "venv" ]; then
-        virtualenv venv
+if [ -z ${WORKSPACE+x} ]; then
+    # Path to virtualenv cmd installed by pip
+    # /usr/local/bin/virtualenv
+    PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
+    if [ ! -d "venv" ]; then
+            virtualenv venv
+    fi
+    . venv/bin/activate
+else
+    # Alternatively, $TRAVIS_REPO_SLUG could be utilized here to provide name.
+    export JOB_NAME="ci-stat"
 fi
-. venv/bin/activate
 pip install -r requirements.txt -r test/test_requirements.txt --cache-dir /tmp/$JOB_NAME
+
 
 ########################
 # Step 2: Execute Test
