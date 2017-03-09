7010d6cee71c02855bd744c26bd285b89e3391ec
maxwu <maxwunj@gmail.com>
2017-03-10 00:47:43 +1300

    update travis and circle with -x to see bash dbg line info

--- test/nosetests_ci-stat.sh
+++ test/nosetests_ci-stat.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+########################
 # Step 1: Setup virtualenv
+########################
 
 # Path to virtualenv cmd installed by pip
 # /usr/local/bin/virtualenv
@@ -11,10 +12,14 @@ fi
 . venv/bin/activate
 pip install -r requirements.txt -r test/test_requirements.txt --cache-dir /tmp/$JOB_NAME
 
+########################
 # Step 2: Execute Test
+########################
 nosetests --with-xunit --all-modules --traverse-namespace --with-xcoverage --cover-package=me.maxwu --cover-inclusive --logging-level=INFO --debug=me.maxwu -s -v --xunit-file ci-stat_nose_xunit.xml --cover-html ./test
 
+########################
 # Step 3: PyLint
+########################
 cd src
 pylint -f parseable -d I0011,R0801 me.maxwu | tee ../pylint.out
 cd ..
