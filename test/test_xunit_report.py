f9cc36712e028a225497551ddc83e9ede1ad058a
maxwu <maxwunj@gmail.com>
2017-04-17 01:21:16 +1200

    update package path

---
+++ test/test_xunit_report.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'maxwu'
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
