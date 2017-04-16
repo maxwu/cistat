0cf1d72de3eaee46c0c53da83d61e07f8b79aa7d
maxwu <maxwunj@gmail.com>
2017-04-17 01:22:24 +1200

    refactor xunit report for case sorting

--- src/me/maxwu/cistat/xunit_report.py
+++ src/me/maxwu/cistat/xunit_report.py
@@ -2,17 +2,66 @@
 # -*- coding: utf-8 -*-
 __author__ = 'maxwu'
 
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+import json
 
 class Xunitrpt(object):
     """ The XUnit Test Report Class.
     Which represents an XML file as a test report in XUnit format.
+    The XSD refers to https://github.com/apache/maven-surefire/blob/master/maven-surefire-plugin/src/site/resources/xsd/surefire-test-report.xsd
     """
 
-    def __init__(self):
-        """Primary constructor with string content
+    def __init__(self, xunit=None):
+        self.case_dict = {}
+        self.accumulate_xunit(xunit)
+
+    def get_cases(self):
+        return self.case_dict.copy()
+
+    def get_case(self, tcname=None):
+        if not tcname:
+            return {}
+        return self.case_dict[tcname]
+
+    def get_cases_in_rate(self, reverse=False):
+        return sorted(self.case_dict.items(), lambda x, y: cmp(x[1]['rate'], y[1]['rate']), reverse=reverse)
+
+    def __str__(self):
+        return json.dumps(self.case_dict, indent=2)
+
+    def accumulate_xunit(self, xunit=None):
+        """ Get test results in dict
+        :param xunit: XUnit in a string
+        :param case_dict: the dict to add current xunit data onto
+        :return: dict of test case {'pass': pass_count, 'fail': failure_count
         """
-        pass
 
-    pass
+        if not xunit:
+            # Nothing to accumulate, return original case dict.
+            return self.case_dict
 
+        root = ET.fromstring(xunit)
+
+        for e in root.iter('testcase'):
+            tcname = e.get('classname') + '.' + e.get('name')
+            if tcname not in self.case_dict:
+                self.case_dict[tcname] = {'pass': 0, 'fail': 0, 'skipped': 0, 'sum': 0, 'rate': 0}
+
+            self.case_dict[tcname]['sum'] += 1
+
+            tags = [child.tag for child in e]
+            if 'failure' in tags or 'error' in tags:
+                self.case_dict[tcname]['fail'] += 1
+            elif 'skipped' in tags:
+                self.case_dict[tcname]['skipped'] += 1
+            else:
+                self.case_dict[tcname]['pass'] += 1
+
+            self.case_dict[tcname]['rate'] = (self.case_dict[tcname]['pass'] + 0.0)/self.case_dict[tcname]['sum']
+
+        # For cascade using.
+        return self
 
--- test/test_xunit_report.py
+++ test/test_xunit_report.py
@@ -3,12 +3,94 @@
 __author__ = 'maxwu'
 
 import unittest
+import json
+from me.maxwu.cistat.xunit_report import Xunitrpt
+from me.maxwu.cistat import config
 
+class TestXunitrpt(unittest.TestCase):
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_xunit_DriverFactoryTest_Sample(self):
+        with open('/'.join([config.get_root(),
+                            'test',
+                            'resources',
+                            'TEST-org.maxwu.jrefresh.selenium.DriverFactoryTest.xml']
+        )) as f:
+            xml = f.read()
 
+        cases = Xunitrpt(xunit=xml).get_cases()
+        print "Pretty format case dict:\n{}".format(json.dumps(cases, indent=2))
+
+        self.assertEqual(4, len(cases))
+        # There are no failure cases in this given sample report.
+        self.assertEqual(0, len([case for case in cases if cases[case]['fail']]))
+
+    # TEST-org.maxwu.jrefresh.selenium.TemperatureConverterTest
+    def test_xunit_TemperatureConverterTest_Sample(self):
+        with open('/'.join([
+            config.get_root(),
+            'test',
+            'resources',
+            'TEST-org.maxwu.jrefresh.selenium.TemperatureConverterTest.xml'
+        ])) as f:
+            xml = f.read()
+
+        cases = Xunitrpt(xunit=xml).get_cases()
+        print "Pretty format case dict:\n{}".format(json.dumps(cases, indent=2))
+
+        self.assertEqual(49, len(cases))
+        # There are 2 failure cases in this given sample report.
+        self.assertEqual(2, len([case for case in cases if cases[case]['fail']]))
+
+    def test_accumulate(self):
+        xunit1 = '''
+            <testsuite tests="2" failures="0" name="org.maxwu.jrefresh.selenium.DriverFactoryTest" time="21.377" errors="0" skipped="0">
+                <testcase classname="org.maxwu.jrefresh.selenium.DriverFactoryTest" name="quitDriverReEntryTest" time="3.496"/>
+                <testcase classname="org.maxwu.jrefresh.selenium.DriverFactoryTest" name="quitDriverTest" time="2.628"/>
+            </testsuite>'''
+
+        xunit2 = '''
+            <testsuite tests="2" failures="1" name="org.maxwu.jrefresh.selenium.DriverFactoryTest" time="21.377" errors="0" skipped="0">
+                <testcase classname="org.maxwu.jrefresh.selenium.DriverFactoryTest" name="quitDriverReEntryTest" time="3.496"/>
+                <testcase classname="org.maxwu.jrefresh.selenium.DriverFactoryTest" name="quitDriverTest" time="2.628">
+                    <failure message="Faked"/>
+                </testcase>
+            </testsuite>'''
+
+        cases = Xunitrpt()
+        print("Empty xunit report: {}".format(cases))
+        self.assertEquals(len(cases.get_cases()), 0)
+
+        cases.accumulate_xunit(xunit1).accumulate_xunit(xunit2)
+        print("Cascaded xunit report: {}".format(cases))
+        self.assertEquals(cases.get_case('org.maxwu.jrefresh.selenium.DriverFactoryTest.quitDriverTest')['fail'], 1)
+        self.assertEquals(cases.get_case('org.maxwu.jrefresh.selenium.DriverFactoryTest.quitDriverTest')['pass'], 1)
+        self.assertEquals(cases.get_case('org.maxwu.jrefresh.selenium.DriverFactoryTest.quitDriverReEntryTest')['fail'], 0)
+        self.assertEquals(cases.get_case('org.maxwu.jrefresh.selenium.DriverFactoryTest.quitDriverReEntryTest')['pass'], 2)
+
+        cases_in_rate = cases.get_cases_in_rate()
+        print("Cases in rate: {}".format(json.dumps(cases_in_rate, indent=2)))
+        expected = [
+            (
+                "org.maxwu.jrefresh.selenium.DriverFactoryTest.quitDriverTest",
+                {
+                    "fail": 1,
+                    "sum": 2,
+                    "skipped": 0,
+                    "rate": 0.5,
+                    "pass": 1
+                }
+            ),
+            (
+                "org.maxwu.jrefresh.selenium.DriverFactoryTest.quitDriverReEntryTest",
+                {
+                    "fail": 0,
+                    "sum": 2,
+                    "skipped": 0,
+                    "rate": 1.0,
+                    "pass": 2
+                }
+            )]
+        self.assertListEqual(cases_in_rate, expected)
 
 if __name__ == '__main__':
     unittest.main()
