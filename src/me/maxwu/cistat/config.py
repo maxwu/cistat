42221a037d3bf9c0d9c2dcea2cf6e7a0c1d8556a
maxwu <maxwunj@gmail.com>
2017-04-25 21:22:46 +1200

    Default token to None

--- src/me/maxwu/cistat/config.py
+++ src/me/maxwu/cistat/config.py
@@ -6,8 +6,10 @@ __author__ = 'maxwu'
 import yaml
 import os
 try:
+    # Python 3
     from collections import ChainMap
 except ImportError:
+    # Python 2.7
     from chainmap import ChainMap
 from me import ROOT_DIR
 import logging
@@ -19,10 +21,15 @@ HOME_PATH = os.path.expanduser('~')
 CONFIG_PATH = '/'.join([HOME_PATH, '.cistat'])
 CACHE_PATH = '/'.join([CONFIG_PATH, 'cache'])
 CONFIG_YAML = '/'.join([CONFIG_PATH, 'config.yaml'])
-DEFAULTS = {'cache_enable': 'true', 'cache_path': CACHE_PATH}
+DEFAULTS = {'cache_enable': 'true', 'cache_path': CACHE_PATH, 'circleci_api_token': None}
 
 
 def get_cfg(path=None):
+    """
+    Read configuration items in order OS environment variables > config.yaml in path parameter > default.
+    :param path: The YAML file of config, if path is None, use ~/.cistat/config.yaml by default
+    :return: ChainMap of configuration items
+    """
     # By default, the main module shall search config.yaml in app root dir.
     if not path:
         path = CONFIG_YAML
@@ -43,6 +50,9 @@ def get_root():
 
 
 def get_circleci_token(path=None):
+    """
+    Token is sometimes not necessary on F/OSS projects if the flag is enabled already on them. 
+    """
     return get_cfg(path)['circleci_api_token']
 
 
