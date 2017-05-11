14825ec2f6ad07d2444f1e409aa4b05705c1053a
maxwu <maxwunj@gmail.com>
2017-05-11 12:06:44 +1200

    fix UT naming bug rpts

--- test/test_Xunitrpt_class_level.py
+++ test/test_Xunitrpt_class_level.py
@@ -21,20 +21,20 @@ class TestXunitrptCls(unittest.TestCase):
             xml = f.read()
         self.rptf = Xunitrpt(xunit=xml)
 
-        # xunit1 = '''
-        #             <testsuite tests="2" failures="0" name="org.maxwu.jrefresh.selenium.DriverFactoryTest" time="21.377" errors="0" skipped="0">
-        #                 <testcase classname="org.maxwu.jrefresh.selenium.DriverFactoryTest" name="quitDriverReEntryTest" />
-        #                 <testcase classname="org.maxwu.jrefresh.selenium.DriverFactoryTest" name="quitDriverTest" />
-        #             </testsuite>'''
-        # self.rpts = Xunitrpt(xunit=xunit1)
+        xunit1 = '''
+                     <testsuite tests="2" failures="0" name="org.maxwu.jrefresh.selenium.DriverFactoryTest" time="21.377" errors="0" skipped="0">
+                        <testcase classname="org.maxwu.jrefresh.selenium.DriverFactoryTest" name="quitDriverReEntryTest" />
+                        <testcase classname="org.maxwu.jrefresh.selenium.DriverFactoryTest" name="quitDriverTest" />
+                    </testsuite>'''
+        self.rpts = Xunitrpt(xunit=xunit1)
 
     def tearDown(self):
         # Nothing to collect for current test
         pass
 
     def test_class_level(self):
-        self.cls_rptf = self.rpts.get_class_rpt()
-        self.assertEqual(1, len(self.cls_rptf))
+        self.cls_rpts = self.rpts.get_class_rpt()
+        self.assertEqual(1, len(self.cls_rpts))
         expected_dict = {
             "org.maxwu.jrefresh.selenium.DriverFactoryTest": {
                 "rate": 1.0,
@@ -45,8 +45,8 @@ class TestXunitrptCls(unittest.TestCase):
                 "time": 0.0
             }
         }
-        self.assertDictEqual(expected_dict, self.cls_rptf.get_cases(), "Class level accumulation failed")
-        print("**Dump Class Level XUnit Report**\n{}".format(self.cls_rptf.dump()))
+        self.assertDictEqual(expected_dict, self.cls_rpts.get_cases(), "Class level accumulation failed")
+        print("**Dump Class Level XUnit Report**\n{}".format(self.cls_rpts.dump()))
 
 
 if __name__ == '__main__':
