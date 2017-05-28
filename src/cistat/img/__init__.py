c495048532a5514ca5624cdb988c3fed365e2f9e
maxwu <maxwunj@gmail.com>
2017-05-28 22:18:50 +1200

    init imgcat pkg

---
+++ src/cistat/img/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Package to show Image in Terminal
+ It is in separate package and would be further maintained in a new repo later.
+ 
+ .. moduleauthor:: Max Wu <http://maxwu.me>
+ .. References::
+    ** iTerm2 imgcat **: https://www.iterm2.com/documentation-images.html 
+    ** Bash Shell Imgcat **: https://github.com/maxwu/iterm2_greeting_with_weather_img 
+"""
+
+from iterm2_imgcat import imgcat
---
+++ src/cistat/img/iterm2_imgcat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Show Image inline with iTerm2
+  The extended protocol to show image in terminal window is described in iterm2 imgcat document as reference item.
+  A base64 encoded string will be interpreted as a picture with configured properties.
+
+ .. moduleauthor:: Max Wu <http://maxwu.me>
+ .. Reference:: https://www.iterm2.com/documentation-images.html 
+"""
+
+import os
+from base64 import b64encode
+from sys import stdout
+
+
+def imgcat(width=None, height=None, preserve_aspect_ratio=False, inline=True, img_path=None):
+    pass
+
+if __name__ == "__main__":
+    pass
